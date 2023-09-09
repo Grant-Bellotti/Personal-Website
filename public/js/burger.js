a4980ded8423295d27152cda8ae9e6dabac235df
Grant Bellotti <55765767+Grant-Bellotti@users.noreply.github.com>
2023-09-09 13:50:14 -0700

    Added About me page and some more details around the website

---
+++ public/js/burger.js
@@ -0,0 +1,22 @@
+let burgerBtn = document.querySelector(".burger-menu-btn");
+let burgerMenu = document.querySelector(".burger-menu");
+
+let isBurgerOpen = false;
+
+burgerBtn.onclick = function () {
+  if (!isBurgerOpen) {
+      burgerMenu.style.display = "block";
+      burgerBtn.style.backgroundPosition = "center left 50px, center";
+      isBurgerOpen = true;
+  }
+  else if (isBurgerOpen) {
+      burgerMenu.style.display = "none";
+      burgerBtn.style.backgroundPosition = "center, center left 50px";
+      isBurgerOpen = false;
+  }
+}
+/*
+$(document).ready(function(){
+  
+});
+*/
